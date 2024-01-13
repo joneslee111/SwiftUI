10045978958e36fe87c7817b73b384e29590772f
lee <leejones111@hotmail.co.uk>
2024-01-13 01:04:18 +0000

    day04Checkpoint2 completed

---
+++ HackingWithSwift/day04Checkpoint2.playground/Contents.swift
@@ -0,0 +1,8 @@
+// create an array of strings
+// print the number of items in the array
+let arsenal = ["Saka", "Jesus", "Martinelli", "Hazard", "Smith-Rowe"]
+print(arsenal.count)
+
+// print the number of unique items in the array
+let arsenalSet = Set(arsenal)
+print(arsenalSet)
